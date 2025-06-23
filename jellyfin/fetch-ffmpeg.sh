8cfcb098a55f9f9b6be501546acbb7661b25c1fe
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-06-23 19:57:32 +0200

    jellyfin: Correct ffmpeg path

--- jellyfin/fetch-ffmpeg.sh
+++ jellyfin/fetch-ffmpeg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -q -O- https://github.com/wolfi-dev/os/raw/refs/heads/main/ffmpeg.yaml | \
-yq '.package.epoch += 1' | \
-yq '.environment.contents.packages += ["libdrm-dev", "libva-dev"]' | \
-yq 'del(.test)' | \
-yq 'del(.subpackages[].test)' | \
-sed 's/--enable-libzmq/--enable-libzmq \\\n        --enable-vaapi \\\n        --enable-libdrm\n/' \
+wget -q -O- https://github.com/wolfi-dev/os/blob/main/ffmpeg-7.yaml | \
+  yq '.package.epoch += 1' | \
+  yq '.environment.contents.packages += ["libdrm-dev", "libva-dev"]' | \
+  yq 'del(.test)' | \
+  yq 'del(.subpackages[].test)' | \
+  sed 's/--enable-libzmq/--enable-libzmq \\\n        --enable-vaapi \\\n        --enable-libdrm\n/' \
 > ffmpeg.melange.yaml
